b595f67f640cbaaf319e47e168f25c2b42ebaeee
Magnus Afiawo <afiawomagkatalyst@gmail.com>
2026-01-11 14:06:11 +0000

    props update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from "./components/UserProfile";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
@@ -15,6 +16,8 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
       <Footer />
 
       <div>
