40501fb543df8d18174e0c73b968e981beb8c66c
Magnus Afiawo <afiawomagkatalyst@gmail.com>
2026-01-18 19:03:37 +0000

    style edit

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ const UserProfile = (props) => {
     <div
       style={{ border: "1px solid gray", padding: "8px 10px", margin: "10px" }}
     >
-      <h2 style={{ color: "orange" }}>{props.name}</h2>
+      <h2 style={{ color: "blue" }}>{props.name}</h2>
       <p>
         Age: <span style={{ fontWeight: "bold" }}>{props.age}</span>
       </p>
