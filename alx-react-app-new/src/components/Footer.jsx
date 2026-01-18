a07eed67df048ffd7ab960012fe25ad8db8be84d
Magnus Afiawo <afiawomagkatalyst@gmail.com>
2026-01-18 19:01:49 +0000

    inline styles for components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        margin: "10px",
+        padding: "8px 10px",
+        backgroundColor: "#00072D",
+      }}
+    >
+      <p style={{ fontStyle: "italic", color: "#fff" }}>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,17 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        margin: "10px",
+        padding: "8px 10px",
+        backgroundColor: "#00072D",
+      }}
+    >
+      <p style={{ color: "blue" }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
