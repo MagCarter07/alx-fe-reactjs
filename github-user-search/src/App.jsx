789dcc615883709f5b5c560713ad00f080effdb2
Magnus Afiawo <afiawomagkatalyst@gmail.com>
2026-01-30 15:44:37 +0000

    initial commit for github user search project

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,11 @@
+import SearchPlaceholder from "./components/SearchPlaceholder";
+
+function App() {
+  return (
+    <div>
+      <SearchPlaceholder />
+    </div>
+  );
+}
+
+export default App;
---
+++ github-user-search/src/components/SearchPlaceholder.jsx
@@ -0,0 +1,10 @@
+const SearchPlaceholder = () => {
+  return (
+    <div>
+      <h2>GitHub User Search</h2>
+      <p>Search functionality coming soon...</p>
+    </div>
+  );
+};
+
+export default SearchPlaceholder;
