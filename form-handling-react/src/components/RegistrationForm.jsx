eba518a9b789fb4e29344996b25adefee9badf18
Magnus Afiawo <afiawomagkatalyst@gmail.com>
2026-02-21 09:57:56 +0000

    initial commit

---
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+
+function RegistrationForm() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const validate = () => {
+    let newErrors = {};
+
+    if (!username.trim()) {
+      newErrors.username = "Username is required";
+    }
+
+    if (!email.trim()) {
+      newErrors.email = "Email is required";
+    }
+
+    if (!password.trim()) {
+      newErrors.password = "Password is required";
+    }
+
+    return newErrors;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const validationErrors = validate();
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length === 0) {
+      console.log("User Registered:", {
+        username,
+        email,
+        password,
+      });
+
+      alert("Registration successful!");
+
+      setUsername("");
+      setEmail("");
+      setPassword("");
+    }
+  };
+
+  return (
+    <div style={{ maxWidth: "400px", margin: "40px auto" }}>
+      <h2>User Registration (Controlled)</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username</label>
+          <br />
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          {errors.username && <p style={{ color: "red" }}>{errors.username}</p>}
+        </div>
+
+        <div>
+          <label>Email</label>
+          <br />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
+        </div>
+
+        <div>
+          <label>Password</label>
+          <br />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {errors.password && <p style={{ color: "red" }}>{errors.password}</p>}
+        </div>
+
+        <br />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default RegistrationForm;
