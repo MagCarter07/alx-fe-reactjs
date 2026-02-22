daffdad3541d86ac0ff76b51756f257df0918b67
Magnus Afiawo <afiawomagkatalyst@gmail.com>
2026-02-22 15:20:02 +0000

    useAuth addition in ProtectedRoutes.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,9 +1,8 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
-
 import Home from "./pages/Home";
 import Profile from "./components/Profile";
 import BlogPost from "./pages/BlogPost";
@@ -13,29 +12,31 @@ function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
-    <div className="min-h-screen bg-gray-100">
-      <Navbar isAuthenticated={isAuthenticated} />
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-100">
+        <Navbar isAuthenticated={isAuthenticated} />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-        <Route
-          path="/login"
-          element={<Login setIsAuthenticated={setIsAuthenticated} />}
-        />
+          <Route
+            path="/login"
+            element={<Login setIsAuthenticated={setIsAuthenticated} />}
+          />
 
-        <Route
-          path="/profile/*"
-          element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/profile/*"
+            element={
+              <ProtectedRoute isAuthenticated={isAuthenticated}>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route path="/blog/:id" element={<BlogPost />} />
-      </Routes>
-    </div>
+          <Route path="/blog/:id" element={<BlogPost />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
